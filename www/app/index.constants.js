8808f44eac666760ac8d1a9240288ef99dcf5f87
Amitesh Kumar <amiteshhh,nitk@gmail.com>
2017-01-14 19:15:49 +0530

    rest end point to heroku

--- www/app/index.constants.js
+++ www/app/index.constants.js
@@ -5,7 +5,8 @@
 
     angular.module(moduleName)
         .value('APP_CONFIG', {
-            SERVER_URL: 'http://localhost:1337',
+            //SERVER_URL: 'http://localhost:1337',
+            SERVER_URL: 'https://obscure-forest-59301.herokuapp.com',
             REST_ENDPOINT: '',
         });
 
