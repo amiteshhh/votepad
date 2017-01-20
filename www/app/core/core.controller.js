53817da692ab1c075579751a38c153aa3c529014
Amitesh Kumar <amiteshhh,nitk@gmail.com>
2017-01-21 03:13:19 +0530

    sails lib added

--- www/app/core/core.controller.js
+++ www/app/core/core.controller.js
@@ -8,6 +8,10 @@
 
     Ctrl.$inject = [];
     function Ctrl() {
-       
+        var socketUrl = 'http://localhost:1337';
+        var socket = io.sails.connect(socketUrl);
+        //io.sails.url = socketUrl;
+        socket.get('/event', function (resData, jwres) { console.log('socekt get', resData); });
+        socket.on('event', function (event) { console.log('event received socket', event); });
     }
 })();
