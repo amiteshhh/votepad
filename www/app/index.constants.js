709ab4d10e1a26722dee1c4d5018217d5f93e813
Amitesh Kumar <amiteshhh,nitk@gmail.com>
2017-01-25 23:56:46 +0530

    server api address update

--- www/app/index.constants.js
+++ www/app/index.constants.js
@@ -6,7 +6,7 @@
     angular.module(moduleName)
         .value('APP_CONFIG', {
             //SERVER_URL: 'http://localhost:1337',
-            SERVER_URL: 'https://obscure-forest-59301.herokuapp.com',
+            SERVER_URL: 'https://votepadserver.herokuapp.com',
             REST_ENDPOINT: '',
             OTP_ENABLED: false,
             OTP_URL:'https://www.cognalys.com/api/v1/otp/?app_id=ab2e8e8a22684b6f92b003e&access_token=8e359af247db3d466ee5fdbce2b841bcc180d457&mobile=+91',
